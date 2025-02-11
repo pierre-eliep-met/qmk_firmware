d080e4839fe6601c2891e3508acf3b40a388593d
Pierre-Elie Personnaz <pierre-elie.personnaz@metroscope.tech>
2025-02-11 14:52:40 +0100

    mts modifs
    Signed-off-by: Pierre-Elie Personnaz <pierre-elie.personnaz@metroscope.tech>

--- keyboards/beekeeb/piantor_pro/keymaps/ergol/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/ergol/keymap.c
@@ -30,37 +30,80 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 1000) {
+    if (timer_elapsed(alt_tab_timer) > 500) {
       unregister_code(KC_LALT);
       is_alt_tab_active = false;
     }
   }
 }
 
+// Initialize variable holding the binary
+// representation of active modifiers.
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Store the current modifier state in the variable for later reference
+    mod_state = get_mods();
+    switch (keycode) {
+
+    case KC_BSPC:
+        {
+        // Initialize a boolean variable that keeps track
+        // of the delete key status: registered or not?
+        static bool delkey_registered;
+        if (record->event.pressed) {
+            // Detect the activation of either shift keys
+            if (mod_state & MOD_MASK_SHIFT) {
+                // First temporarily canceling both shifts so that
+                // shift isn't applied to the KC_DEL keycode
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                // Update the boolean variable to reflect the status of KC_DEL
+                delkey_registered = true;
+                // Reapplying modifier state so that the held shift key(s)
+                // still work even after having tapped the Backspace/Delete key.
+                set_mods(mod_state);
+                return false;
+            }
+        } else { // on release of KC_BSPC
+            // In case KC_DEL is still being sent even after the release of KC_BSPC
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+        }
+        // Let QMK process the KC_BSPC keycode as usual outside of shift
+        return true;
+    }
+
+    }
+    return true;
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_ESC,
+       KC_TAB,    KC_Q,    KC_C,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSPC,
+      KC_LCTL,    KC_Z,    KC_X,    KC_W,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,  KC_SPC, TT(1),       KC_ENT,LCTL(KC_BSPC),KC_RALT
+                                        KC_LGUI,LALT_T(KC_SPC),TT(1),   KC_ENT,LCTL(KC_BSPC),KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      ALT_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_PGUP, TO(0),
+      ALT_TAB, XXXXXXX, XXXXXXX, MS_BTN2,   MS_UP, MS_BTN1,                      MS_WHLU, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  TO(0),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,LCTL(KC_A),LCTL(KC_S),XXXXXXX,TT(2),XXXXXXX,			 XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN, KC_CAPS, 
+      KC_LSFT,LCTL(KC_A),LCTL(KC_S),MS_LEFT,MS_DOWN,MS_RIGHT,		                 MS_WHLD, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN, KC_CAPS, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- KC_LCTL,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V),XXXXXXX,     	         XXXXXXX, MS_WHLL, MS_WHLU, MS_WHLD, MS_WHLR, KC_BSPC,
+ KC_LCTL,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V),TT(2),     	               XXXXXXX, MS_WHLL, KC_BSPC, MS_WHLR, XXXXXXX, KC_LALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_SPC,   TO(0),     KC_ENT,LCTL(KC_BSPC), KC_LCTL
+                                        KC_LGUI,LALT_T(KC_SPC),TO(0),  KC_ENT,LCTL(KC_BSPC), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
   
@@ -68,13 +111,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      ALT_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX, TO(0),
+      ALT_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_AMPR, KC_PAST, KC_PSLS, XXXXXXX, TO(0),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,			    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX, 
+      KC_LSFT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,			                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     	         XXXXXXX, KC_MINS, KC_COMM,  KC_DOT, KC_SLSH, KC_BSPC,
+      KC_LCTL, XXXXXXX, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,     	               KC_PDOT, KC_PPLS, KC_PMNS, KC_PCMM, XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_SPC,   TO(1),     KC_ENT,LCTL(KC_BSPC), KC_LCTL
+                                        KC_LGUI,LALT_T(KC_SPC),TO(1),  KC_ENT,LCTL(KC_BSPC), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 };
