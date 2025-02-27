3b2f969710b8afce908cbbaab28170dd92980682
Pierre-Elie Personnaz <pierre-elie.personnaz@metroscope.tech>
2025-02-27 09:54:06 +0100

    moved backspace to right hand
    Signed-off-by: Pierre-Elie Personnaz <pierre-elie.personnaz@metroscope.tech>

--- keyboards/beekeeb/piantor_pro/keymaps/miryoku/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/miryoku/keymap.c
@@ -13,30 +13,29 @@ const key_override_t *key_overrides[] = {
   &word_delete_key_override,
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, MT(MOD_RSFT, KC_CAPS),
+OSM(MOD_LSFT),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, MT(MOD_RSFT, KC_CAPS),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSPC,
+MT(MOD_LCTL,KC_BSPC),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                              TT(1),LALT_T(KC_SPC),MT(MOD_LGUI,KC_ENT),LCTL(KC_BSPC),MT(MOD_RALT,KC_LSFT),TT(2)
+                              MT(MOD_LGUI,KC_ENT),LALT_T(KC_SPC),TT(1),TT(2),LCTL(KC_BSPC),MT(MOD_RALT,KC_ENT)
                                       //`--------------------------'  `--------------------------'
   ),
 
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB, XXXXXXX,LCTL(KC_W),KC_VOLU, XXXXXXX, KC_BRIU,                      MS_WHLU, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  TO(0),
+    KC_TAB, XXXXXXX,LCTL(KC_W),KC_VOLU,LALT(KC_F4), KC_BRIU,                      MS_WHLU, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  TO(0),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    KC_LSFT,LCTL(KC_A),LCTL(KC_S),KC_VOLD,  KC_F2, KC_BRID,		                   MS_WHLD, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN, KC_CAPS, 
+OSM(MOD_LSFT),LCTL(KC_A),LCTL(KC_S),KC_VOLD,KC_F2, KC_BRID,		                   MS_WHLD, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN, KC_CAPS, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- KC_LCTL,LCTL(KC_Z),LCTL(KC_X),KC_MUTE,LCTL(KC_V),LCTL(KC_Y),     	      MS_WHLL,LCTL(KC_LEFT),XXXXXXX,LCTL(KC_RIGHT),MS_WHLR,KC_BSPC,
+MT(MOD_LCTL,KC_BSPC),LCTL(KC_Z),LCTL(KC_X),KC_MUTE,LCTL(KC_V),LCTL(KC_Y),     	 MS_WHLL,LCTL(KC_LEFT),XXXXXXX,LCTL(KC_RIGHT),MS_WHLR,KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            TT(1),LALT_T(KC_SPC),MT(MOD_LGUI,KC_ENT),LCTL(KC_BSPC),MT(MOD_RALT,KC_LSFT),TT(2)
+                            MT(MOD_LGUI,KC_ENT),LALT_T(KC_SPC),TT(1),TT(2),LCTL(KC_BSPC),MT(MOD_RALT,KC_ENT)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -44,13 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB, XXXXXXX,     KC_7,    KC_8,    KC_9, XXXXXXX,                      XXXXXXX, KC_AMPR, KC_PAST, XXXXXXX, XXXXXXX,   TO(0),
+      KC_TAB, XXXXXXX,     KC_7,    KC_8,    KC_9, KC_PERC,                      XXXXXXX, KC_AMPR, KC_PAST, XXXXXXX, XXXXXXX,   TO(0),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_0,    KC_1,    KC_2,    KC_3, XXXXXXX,                      XXXXXXX, KC_PPLS, KC_PMNS, KC_PSLS, XXXXXXX, XXXXXXX,
+OSM(MOD_LSFT),    KC_0,    KC_1,    KC_2,    KC_3, XXXXXXX,                      XXXXXXX, KC_PPLS, KC_PMNS, KC_PSLS, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX,    KC_4,    KC_5,	   KC_6, XXXXXXX,     	               KC_PDOT, XXXXXXX, XXXXXXX,  KC_DOT, XXXXXXX, KC_BSPC,
+MT(MOD_LCTL,KC_BSPC), XXXXXXX,    KC_4,    KC_5,	   KC_6, XXXXXXX,     	       KC_PDOT, XXXXXXX, XXXXXXX,  KC_DOT, XXXXXXX, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            TT(1),LALT_T(KC_SPC),MT(MOD_LGUI,KC_ENT),LCTL(KC_BSPC),MT(MOD_RALT,KC_LSFT),TT(2)
+                            MT(MOD_LGUI,KC_ENT),LALT_T(KC_SPC),TT(1),TT(2),LCTL(KC_BSPC),MT(MOD_RALT,KC_ENT)
                                       //`--------------------------'  `--------------------------'
   ),
 };
